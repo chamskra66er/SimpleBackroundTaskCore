6d95614722be0c1c6a18129db4687dbf5042a2af
Anatoliy Novikov <chamskra66er@gmail.com>
2020-06-13 19:01:41 +0300

    added model Domains

---
+++ SimpleBackgroundTask/Models/Domains.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleBackgroundTask.Models
+{
+    public class Domains
+    {
+        public int Id { get; set; }
+        public string ServerName{get; set;}
+        public bool Status { get; set; }
+        public string Url { get; set; }
+    }
+}
