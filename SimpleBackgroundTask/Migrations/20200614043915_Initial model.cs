c4b487a83c4d00d8f1a1dcc6e8b6107aba947445
Anatoliy Novikov <chamskra66er@gmail.com>
2020-06-14 07:40:59 +0300

    added migration

---
+++ SimpleBackgroundTask/Migrations/20200614043915_Initial model.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SimpleBackgroundTask.Migrations
+{
+    public partial class Initialmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Domain",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServerName = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    Url = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Domain", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Domain");
+        }
+    }
+}
